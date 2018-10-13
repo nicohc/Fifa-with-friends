a3674218e4fd5ce255a6ca5a92f9d11825e1e8ba
Nico <nico1099@hotmail.fr>
2018-10-13 14:49:30 +0200

    Debug create new player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,14 +7,6 @@ class PlayersController < ApplicationController
     @player = Player.new(players_params)
     @player.points = 0
 
-    @player.win = 0
-    @player.win_prol = 0
-    @player.win_peno = 0
-    @player.lose = 0
-    @player.lose_prol = 0
-    @player.lose_peno = 0
-    @player.draw = 0
-
     if @player.save
       redirect_to @player
     else
