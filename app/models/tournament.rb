4c0b354edb336917d0ac570616cff0a71db4743d
Nico <nico1099@hotmail.fr>
2018-10-20 01:33:02 +0200

    debug test

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,6 +1,7 @@
 class Tournament < ApplicationRecord
 
-  has_many :matches
+  has_many :matches, dependent: :destroy,
+    allow_destroy: true
 
   has_many :seasons, dependent: :destroy
   accepts_nested_attributes_for :seasons,
