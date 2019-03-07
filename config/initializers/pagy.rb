3b6694b45a34fb6d0cbb8ad0bfd341192d334202
nico <nico1099@hotmail.fr>
2019-03-07 21:06:39 +0100

    back2

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -33,7 +33,7 @@
 
 # Bootstrap extra: Add nav, responsive and compact helpers and templates for Bootstrap pagination
 # See https://ddnexus.github.io/pagy/extras/bootstrap
-require 'pagy/extras/bootstrap'
+#require 'pagy/extras/bootstrap'
 
 # Bulma extra: Add nav, responsive and compact helpers and templates for Bulma pagination
 # See https://ddnexus.github.io/pagy/extras/bulma
@@ -92,7 +92,7 @@ require 'pagy/extras/bootstrap'
 
 # Instance variables
 # See https://ddnexus.github.io/pagy/api/pagy#instance-variables
-Pagy::VARS[:items] = 50                                  # default
+#Pagy::VARS[:items] = 50                                  # default
 
 
 # Other Variables
